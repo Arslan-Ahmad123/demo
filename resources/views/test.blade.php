a0f131d41ef4ee3710d8932e149281ce01678d67
Arslan-Ahmad123 <arslan.ahmadmusa@gmail.com>
2024-02-29 00:44:26 +0500

    add test route and view

---
+++ resources/views/test.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+
+<body>
+    <h1>Test</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat repudiandae voluptatibus amet, illum quidem
+        adipisci, sit natus aliquam excepturi perspiciatis enim eius odio nisi voluptatem quo assumenda quia neque,
+        consequatur modi tempore at? Iusto atque reiciendis natus quia ullam magnam odio beatae aut, hic deserunt quae
+        obcaecati libero. Ipsam eius quibusdam dolore fugiat impedit illo quos reiciendis? Fuga id et ipsa incidunt quam
+        corrupti ipsam voluptas eum, ea excepturi ducimus harum repudiandae aperiam consequatur velit voluptatum
+        provident, pariatur nesciunt dolorem, impedit sapiente distinctio? Laboriosam maiores molestiae natus excepturi
+        culpa deleniti hic? A velit voluptas non consectetur, iusto in quis tempore aliquid quia, nisi ex dolorum
+        deserunt, repudiandae fuga quidem labore. Exercitationem quis neque sint dolores obcaecati delectus cupiditate
+        ullam, eligendi voluptates libero! Repudiandae esse illum sit mollitia eum officia? Aspernatur, aliquam? Enim
+        ipsam nulla ullam sunt totam, explicabo in ex deleniti ipsa accusamus illo vitae rem sed aperiam ea beatae
+        veniam, autem, laudantium labore fugit sapiente amet quisquam? Dignissimos cum, sequi magnam ea quae illo
+        ratione optio voluptatum odio ullam esse ipsa expedita accusantium aut necessitatibus fugiat deserunt aspernatur
+        recusandae nemo cumque. Iure blanditiis quasi distinctio! Adipisci esse nam eveniet dolore minima, cupiditate
+        quas sequi, blanditiis animi itaque, facilis enim dolores corporis magnam at. Ab quos dolores mollitia. Corporis
+        dolorem vitae asperiores eos voluptates impedit repudiandae animi debitis, odio, hic a quaerat eius. Neque,
+        minima doloribus natus, rerum sint eveniet tempora praesentium vel dicta, numquam aut possimus sunt aliquid.
+        Reprehenderit, voluptatum minus possimus sint porro praesentium dolorem tenetur assumenda inventore, nostrum
+        fugiat iusto, magnam doloremque voluptates asperiores quos! Dolor accusamus, amet totam veritatis tempore rem
+        velit officia! Asperiores ipsa aspernatur fuga sapiente impedit explicabo. Accusantium dolore voluptatem
+        maiores, maxime iste nesciunt illum temporibus quos ipsum, exercitationem quas minima nihil eveniet commodi,
+        culpa laboriosam earum veritatis eius doloribus! Quas, recusandae esse.</p>
+</body>
+
+</html>
